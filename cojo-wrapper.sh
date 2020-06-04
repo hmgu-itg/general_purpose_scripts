61e61059e105eea6c31ac9338f319896a9995fb0
abarysenka <andrei.barysenka@gmail.com>
2020-06-04 10:50:40 +0200

    cojo-wrapper.sh

--- cojo-wrapper.sh
+++ cojo-wrapper.sh
@@ -1,2 +1,26 @@
 #!/usr/bin/bash
 
+function usage {
+    echo ""
+    echo "Usage: $0 -f <PLINK file prefix>"
+    echo "          -o <output prefix>"
+    echo "          -i <input table>"
+    echo "          -w <bp window>"
+    echo "          -k <BED file with known signals>"
+    exit 0
+}
+
+
+OPTIND=1
+while getopts "f:o:i:w:k:" optname; do
+    case "$optname" in
+        "f" ) bfile="${OPTARG}";;
+        "o" ) out="${OPTARG}";;
+        "i" ) input="${OPTARG}";;
+        "w" ) window="${OPTARG}";;
+        "k" ) known="${OPTARG}";;
+        "?" ) usage ;;
+        *) usage ;;
+    esac;
+done
+
