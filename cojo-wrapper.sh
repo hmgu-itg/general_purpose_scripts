1e292bfab7c60043c293c4e6217c997f8ceb05bb
abarysenka <andrei.barysenka@gmail.com>
2020-06-08 17:02:17 +0200

    minor fix

--- cojo-wrapper.sh
+++ cojo-wrapper.sh
@@ -80,7 +80,7 @@ echo >> "$logfile"
 # calling GCTA
 echo -n "Calling GCTA ... " >> "$logfile"
 
-echo "COJO file: " >> "$logfile"
+echo "\nCOJO file: " >> "$logfile"
 fgrep -w "$varid" "$input"|cut -f 2-|sed 's/\t/:/'| grep -v -f <(cut -f 2 "$plinkout"."bim" | cat - <(fgrep -w "$varid" "$input"|cut -f 2,3| tr '\t' ':') | sort|uniq -u) >> "$logfile"
 echo "COND file" >> "$logfile"
 fgrep -v -w "$id" "$plinkout"."bim" >> "$logfile"
