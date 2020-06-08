509ac270f2c9216e993d7c3a451da7ce030e7f59
abarysenka <andrei.barysenka@gmail.com>
2020-06-08 14:24:59 +0200

    Check if phenotype files exist

--- cojo-prepare.sh
+++ cojo-prepare.sh
@@ -64,15 +64,27 @@ id="$chr:$pos"
 suffix="$chr"_"$pos"
 varid="$panel"_"$prot"_"$suffix"
 
+echo "=========================================" >> "$logfile"
+echo "Variant: $prefix $id" >> "$logfile"
+echo >> "$logfile"
+
 #------------------- total number of samples ------------------------------
+$phenofile="$ha_pheno"."$prefix"."txt"
+if [[ ! -f "$phenofile" ]];then
+    echo "ERROR: $phenofile doesn't exist" >> "logfile"
+    continue
+fi
 ha_N=$(cat <(cut -f 1 "$ha_pheno"."$prefix"."txt") <(cut -f 2 -d ' ' "$ha_plink"."fam") | sort|uniq -d|wc -l)
+
+$phenofile="$hp_pheno"/"$panel"/"POMAK"."$panel"."$prot"."txt"
+if [[ ! -f "$phenofile" ]];then
+    echo "ERROR: $phenofile doesn't exist" >> "logfile"
+    continue
+fi
 hp_N=$(cat <(cut -f 1 "$hp_pheno"/"$panel"/"POMAK"."$panel"."$prot"."txt") <(cut -f 2 -d ' ' "$hp_plink"."fam") | sort|uniq -d|wc -l)
 N=$((ha_N+hp_N-nMiss))
 #------------------------------------------------------------------------------------
 
-echo "=========================================" >> "$logfile"
-echo "Variant: $prefix $id" >> "$logfile"
-echo >> "$logfile"
 echo "Total samples: $N" >> "$logfile"
 echo >> "$logfile"
 
