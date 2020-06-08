2c9020bd646eb108467d294f6c3d1a4300a41dfd
abarysenka <andrei.barysenka@gmail.com>
2020-06-08 13:09:07 +0200

    Logging

--- cojo-prepare.sh
+++ cojo-prepare.sh
@@ -42,10 +42,11 @@ hp_plink="/storage/sanger/projects/helic/t144_helic_15x/analysis/HP/missingness/
 ha_pheno="/storage/sanger/projects/helic/t144_helic_15x/analysis/HA/phenotypes/OLINK/MANOLIS"
 hp_pheno="/storage/hmgu/projects/helic/OLINK/HP/phenotypes"
 
-echo "window        : $window" >> "$logfile"
+echo "window        : $window" > "$logfile"
 echo "known signals : $known" >> "$logfile"
 echo "input table   : $input" >> "$logfile"
 echo "output table  : $output" >> "$logfile"
+echo >> "$logfile"
 
 tmpfile=$(mktemp "$PWD"/tmp_cojo.XXXX)
 
@@ -73,7 +74,9 @@ uniprot=$(echo "$uniprot"| perl -lne '@a=split(/,/);print join(",",sort @a);')
 
 start=$((pos-window))
 end=$((pos+window))
+echo "intersectBed using chr=$chr start=$start end=$end" >> "$logfile"
 intersectBed -wb -a <(echo "\"$chr $start $end\""| tr ' ' '\t') -b "$known" | awk -v x="$uniprot" 'BEGIN{FS="\t";OFS="\t";}$8==x{print $0;}' | cut -f 1,3| tr '' '' > "$tmpfile"
+echo >> "$logfile"
 
 # if there are no known signals in the bp window
 nKnown=$(cat "$tmpfile"| wc -l)
