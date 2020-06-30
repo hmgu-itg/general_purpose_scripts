90bbba126380e7c2822613a9a099048789fe3c18
abarysenka <andrei.barysenka@gmail.com>
2020-06-30 17:02:58 +0200

    Finalize cojo.py

--- cojo.py
+++ cojo.py
@@ -8,6 +8,7 @@ import time
 import datetime
 import pandas as pd
 import numpy as np
+from math import sqrt
 
 parser = argparse.ArgumentParser(description="Perform conditional analysis")
 parser.add_argument('--ma','-m', action="store",help="Meta-analysis results")
@@ -94,10 +95,16 @@ a_var=None
 tmp_betas=dict()
 tmp_freqs=dict()
 tmp_alleles=dict()
+tmp_SEs=dict()
 
 AL=dict() # id --> effect allele
+allSE=dict() # id --> SE
+allbeta=dict() # id --> peta
 for index, row in mafile.iterrows():
     AL[row["SNP"]]=row["A1"]
+    allSE[row["SNP"]]=row["se"]
+    allbeta[row["SNP"]]=row["b"]
+
     if var==row["SNP"]:
         beta_var=float(row["b"])
         f_var=float(row["freq"])
@@ -106,6 +113,7 @@ for index, row in mafile.iterrows():
         tmp_betas[row["SNP"]]=float(row["b"])
         tmp_freqs[row["SNP"]]=float(row["freq"])
         tmp_alleles[row["SNP"]]=row["A1"]
+        tmp_SEs[row["SNP"]]=row["se"]
 
 AF=dict() # id --> effect allele AF
 for x in AL:
@@ -124,12 +132,15 @@ for x in AL:
             t=t+1
     AF[x]=c/(2*t)
 
+# data of the conditioning variants
+SEs=[]
 betas=[]
 freqs=[]
 alleles=[]
 
 # betas etc. in correct order
-for x in L2:
+for x in L3:
+    SEs.append(tmp_SEs[x])
     betas.append(tmp_betas[x])
     freqs.append(tmp_freqs[x])
     alleles.append(tmp_alleles[x])
@@ -140,9 +151,11 @@ print(a_var,f_var,beta_var)
 print("COND ALLELES: ",alleles)
 print("COND FREQS  : ",freqs)
 print("COND BETAS  : ",betas)
+print("COND SE     : ",SEs)
 
 # genotype encoded
-df0=pd.DataFrame()
+#df0=pd.DataFrame()
+df=pd.DataFrame()
 for index, row in mapfile.iterrows():
     x=row["ID"]
     a=mafile.loc[mafile.SNP==x,"A1"].values[0]
@@ -154,14 +167,31 @@ print("")
 print(df)
 print("")
 
+nsamples=df.shape[0]
+print('{:=^80}'.format(' N SAMPLES '))
+print("")
+print(nsamples)
+print("")
+
 # remove rows with NaNs
-#df=df0.dropna()
+#df=df.dropna()
 #print('{:=^80}'.format(' GENOTYPE MATRIX '))
 #print("")
 #print(df)
 #print("")
 
 #-------------------------------------------- creating necessary matrices -------------------------------------------------
+
+allD=dict()
+for v in L1:
+    tmp=df[[v]].to_numpy(copy=True)
+    allD[v]=np.dot(np.transpose(tmp),tmp)
+    
+print('{:=^80}'.format(' D '))
+print("")
+for v in L1:
+    print(v,allD[v],sep="\t")
+print("")
 
 # tested variant's genotypes
 X2=df[[var]].to_numpy(copy=True)
@@ -229,6 +259,55 @@ print("")
 print(X)
 print("")
 
+#------------------------------------------------------ calculating conditional beta ----------------------------------------------
+
 b2=beta_var-np.dot(X,betas)
 print('{:=^80}'.format(' OUTPUT '))
 print(ma,str(beta_var),str(b2),sep="\t")
+
+#-------------------------------------------------------------calculating SE ------------------------------------------------------
+tmpL=[]
+for v in L1:
+    tmpL.append((nsamples-1)*allSE[v]*allSE[v]+allD[v]*allbeta[v]*allbeta[v])
+
+yty=np.median(tmpL)
+#yty=np.mean(tmpL)
+print('{:=^80}'.format(' yty '))
+print("")
+print(tmpL)
+print(yty)
+print("")
+
+tmp=np.dot(X11,D1)
+b1=np.dot(tmp,betas)
+print('{:=^80}'.format(' b1 '))
+print("")
+print(b1)
+print("")
+
+tmp=np.dot(np.transpose(b1),D1)
+a2=np.dot(tmp,betas)
+print('{:=^80}'.format(' a2 '))
+print("")
+print(a2)
+print("")
+
+sigma2=(yty-a2-b2*allD[var]*allbeta[var])/(nsamples-len(L1))
+print('{:=^80}'.format(' sigma2 '))
+print("")
+print(sigma2)
+print("")
+
+t1=np.dot(np.transpose(X2),X1)
+t2=np.dot(np.transpose(X1),X2)
+z=D2-np.dot(np.dot(t1,X11),t2)
+print('{:=^80}'.format(' z '))
+print("")
+print(z)
+print("")
+
+se2=sqrt(sigma2*z)/allD[var]
+print('{:=^80}'.format(' SE2 '))
+print("")
+print(se2)
+print("")
