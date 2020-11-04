9200550a5ceed54db0ecb6c863f1f9d69e1aeb26
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-11-04 17:05:29 +0100

    Add functions.sh

--- functions.sh
+++ functions.sh
@@ -1,8 +1,28 @@
 
-function parseCommandLine () {
-    aggs=("$@")
-    for i in "${arr[@]}";
-    do
-	echo "ARG: " $i
+function getvalue () {
+    args=("$@")
+    key=${args[-1]}
+    unset args[-1]
+
+    for ((i=0; i <= ${#args[@]}; i++)); do
+	if [[ "${args[i]}" == "-$key" ]];then
+	    echo "${args[i+1]}"
+	    break
+	fi
     done
+}
+
+
+function parseCommandLine () {
+    declare -a A
+    args=("$@")
+    s=${args[-1]}
+    unset args[-1]
+    for ((i=0; i<${#s}; i++)); do
+	x="${s:$i:1}"
+	z=$(getvalue ${args[@]} $x)
+	A+=("$z")	
+    done
+    
+    echo ${A[@]}
 }
