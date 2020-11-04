93bbc62bcf0d131b974103ac9aa9b1e400b471f1
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-11-04 18:53:07 +0100

    Update quant2bin.sh

--- quant2bin.sh
+++ quant2bin.sh
@@ -4,11 +4,16 @@ source "./functions.sh"
 
 args=("$@")
 
-read -r input output <<<$(parseCommandLine ${args[@]} "io")
+read -r input frac <<<$(parseCommandLine ${args[@]} "if")
 
 echo $input
-echo $output
+echo $frac
+frac1=$(echo 1.0-$frac | bc)
+echo $frac1
 
-
-#function(x,a,b){if (x<a) return(0);if (x <= b & x >= a) return(NA);if (x>b) return(1);}
-#> df$b<-lapply(df$zres,function(x) f(x,quantile(df$zres,0.45),quantile(df$zres,0.55)))
+Rscript -e 'library("data.table")
+df<-read.table("'$input'",sep="\t",header=T)
+f<-function(x,a,b){if (x<a) return(0);if (x <= b & x >= a) return(NA);if (x>b) return(1);}
+df$b<-lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'),quantile(df$zres,'$frac1')))
+print(df[c("ID","b")])
+'
