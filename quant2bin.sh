3da3486967613c15a78bd4f68c7fe08e91adfab0
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-11-10 09:53:54 +0100

    Update quant2bin.sh

--- quant2bin.sh
+++ quant2bin.sh
@@ -6,14 +6,16 @@ args=("$@")
 
 read -r input frac <<<$(parseCommandLine ${args[@]} "if")
 
-echo $input
-echo $frac
-frac1=$(echo 1.0-$frac | bc)
-echo $frac1
+#echo $input
+#echo $frac
+#frac1=$(echo 1.0-$frac | bc)
+#echo $frac1
 
-Rscript -e 'library("data.table")
-df<-read.table("'$input'",sep="\t",header=T)
-f<-function(x,a,b){if (x<a) return(0);if (x <= b & x >= a) return(NA);if (x>b) return(1);}
-df$b<-lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'),quantile(df$zres,'$frac1')))
-print(df[c("ID","b")])
+Rscript -e '#library("data.table")
+df<-read.table("'$input'",sep="\t",header=T,stringsAsFactors=F)
+#f<-function(x,a,b){if (x<a) return(0);if (x <= b & x >= a) return(NA);if (x>b) return(1);}
+f<-function(x,a){if (x<=a) return(0) else return(1);}
+#df$b<-lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'),quantile(df$zres,'$frac1')))
+df$b<-as.numeric(lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'))))
+write.table(df[c("ID","b")],quote=F,sep="\t",row.names=F)
 '
