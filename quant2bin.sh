c9dd93fa7963eafe7028c1acb88675e590e357e8
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-11-10 10:02:15 +0100

    Update quant2bin.sh

--- quant2bin.sh
+++ quant2bin.sh
@@ -12,10 +12,10 @@ read -r input frac <<<$(parseCommandLine ${args[@]} "if")
 #echo $frac1
 
 Rscript -e '#library("data.table")
-df<-read.table("'$input'",sep="\t",header=T,stringsAsFactors=F)
+df<-read.table("'$input'",sep="\t",header=F,stringsAsFactors=F)
 #f<-function(x,a,b){if (x<a) return(0);if (x <= b & x >= a) return(NA);if (x>b) return(1);}
 f<-function(x,a){if (x<=a) return(0) else return(1);}
 #df$b<-lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'),quantile(df$zres,'$frac1')))
 df$b<-as.numeric(lapply(df$zres,function(x) f(x,quantile(df$zres,'$frac'))))
-write.table(df[c("ID","b")],quote=F,sep="\t",row.names=F)
+write.table(df[c("V1","b")],quote=F,sep="\t",row.names=F)
 '
