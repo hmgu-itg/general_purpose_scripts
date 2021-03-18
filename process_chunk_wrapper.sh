30848bb277eab906e4b41034ae81aff4e9d07400
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-18 16:33:35 +0100

    Add SLURM_ARRAY_TASK_ID

--- process_chunk_wrapper.sh
+++ process_chunk_wrapper.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 indir=$1
-n=$2
-t=$3
-pheno=$4
+n=$SLURM_ARRAY_TASK_ID
+t=$2
+pheno=$3
 
 indir=${indir/%/}
-
 total=$(ls $indir/*.vcf.gz| wc -l)
 
 if (( $n > $total ));then
@@ -15,8 +14,6 @@ if (( $n > $total ));then
 fi
 
 fname=$(ls $indir/*.vcf.gz| sort | head -n $n | tail -n 1)
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 "$DIR"/process_chunk.sh -i $fname -t $t -p $pheno
 
