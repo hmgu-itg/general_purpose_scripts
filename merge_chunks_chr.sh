7c6b1b66ace4f75cea2419b868b52755be84ab05
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-19 19:49:27 +0100

    Bug fix

--- merge_chunks_chr.sh
+++ merge_chunks_chr.sh
@@ -6,24 +6,25 @@ indir=${indir%/}
 output="$indir"/merged.vcf.gz
 plout="$indir"/merged
 
-echo "MERGING VCF FILES IN $indir AND CONVERTING TO PLINK FORMAT"
+echo "MERGING VCF FILES IN $indir"
+echo "AND CONVERTING TO PLINK FORMAT"
 echo "INPUT DIR=$indir"
 echo "OUTPUT=$output"
 echo "PLINK OUTPUT PREFIX=$plout"
 echo "-------------------------------------"
 echo ""
 
-list=$(mktemp -t merge_list-XXXXXXX)
-if [[ ! -f $list ]];then
-    echo "ERROR: could not create list file; exit"
+flist=$(mktemp -t merge_list-XXXXXXX)
+if [[ ! -f $flist ]];then
+    echo "ERROR: could not create merging list file; exit"
     exit 1
 fi
 
 for f in $(find "$indir" -maxdepth 1 -mindepth 1 -name "*.vcf.gz");do
-    realpath $f >> list
+    realpath $f >> "$flist"
 done
 
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -f list -Oz -o "$output"
+singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -f "$flist" -Oz -o "$output"
 singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 tabix "$output"
-rm -f list
+rm -f "$flist"
 singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 plink2 --vcf "$output" --make-pgen --out "$plout"
