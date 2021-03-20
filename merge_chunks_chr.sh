59e941d5a608ae96b339aa2c8924b438c419b6b6
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-20 22:38:19 +0100

    Add threads and --allow-overlap

--- merge_chunks_chr.sh
+++ merge_chunks_chr.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 indir=$1
+threads=$2
 
 indir=${indir%/}
 output="$indir"/merged.vcf.gz
 plout="$indir"/merged
 
-echo "MERGING VCF FILES IN $indir"
-echo "AND CONVERTING TO PLINK FORMAT"
-echo "INPUT DIR=$indir"
-echo "OUTPUT=$output"
-echo "PLINK OUTPUT PREFIX=$plout"
+echo "MERGING VCF FILES AND CONVERTING TO PLINK FORMAT"
+echo "INPUT DIR $indir"
+echo "USING THREADS $threads"
+echo "OUTPUT VCF $output"
+echo "PLINK OUTPUT PREFIX $plout"
 echo "-------------------------------------"
 echo ""
 
@@ -24,7 +25,7 @@ for f in $(find "$indir" -maxdepth 1 -mindepth 1 -name "*.vcf.gz");do
     realpath $f >> "$flist"
 done
 
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -f "$flist" -Oz -o "$output"
+singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -a -f "$flist" -Oz -o "$output" --threads "$threads"
 singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 tabix "$output"
 rm -f "$flist"
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 plink2 --vcf "$output" --make-pgen --out "$plout"
+singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 plink2 --vcf "$output" --make-pgen --out "$plout" --threads "$threads"
--- process_dir.sh
+++ process_dir.sh
@@ -6,13 +6,15 @@ c=$SLURM_ARRAY_TASK_ID
 indir=$1
 t=$2
 pheno=$3
+threads=$4
 
 indir=${indir%/}
 
-echo "INPUT DIR=$indir"
-echo "P THRESHOLD=$t"
-echo "PHENOTYPE FILE=$pheno"
-echo "CURRENT CHROM=$c"
+echo "INPUT DIR $indir"
+echo "P THRESHOLD $t"
+echo "THREADS $t"
+echo "PHENOTYPE FILE $pheno"
+echo "CURRENT CHROM $c"
 echo ""
 
 # symlinks go here
@@ -45,7 +47,7 @@ echo ""
 
 ID=$(sbatch --job-name=process_chr"$c" --cpus-per-task=1 --mem-per-cpu=10G --time=10:00:00 -p normal_q --array=1-"$total" -o process_chunk_wrapper_chr"$c"_%A_%a.log -e process_chunk_wrapper_chr"$c"_%A_%a.err /compute/Genomics/software/scripts/general_purpose_scripts/process_chunk_wrapper.sh "$outdir" "$t" "$pheno" "$outdir2"| cut -d ' ' -f 4)
 
-ID=$(sbatch --job-name=merge_chr"$c" --dependency=afterok:$ID --cpus-per-task=1 --mem-per-cpu=10G --time=10:00:00 -p normal_q --array=1 -o merge_chr"$c"_%A_%a.log -e merge_chr"$c"_%A_%a.err /compute/Genomics/software/scripts/general_purpose_scripts/merge_chunks_chr.sh "$outdir2"| cut -d ' ' -f 4)
+ID=$(sbatch --job-name=merge_chr"$c" --dependency=afterok:$ID --cpus-per-task=${threads} --mem=200G --time=10:00:00 -p normal_q --array=1 -o merge_chr"$c"_%A_%a.log -e merge_chr"$c"_%A_%a.err /compute/Genomics/software/scripts/general_purpose_scripts/merge_chunks_chr.sh "$outdir2" "$threads"| cut -d ' ' -f 4)
 
 # cleanup
 
