610324785b4acfd40fcd5eb7e9b8d1a38845f6be
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-21 16:29:31 +0100

    Add collect_stats_chunk.sh

---
+++ collect_stats_chunk.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# using bcftools and qctool2 to collect missingness stats from one VCF file
+#
+###################
+
+indir=$1
+phenofile=$2
+n=$SLURM_ARRAY_TASK_ID
+
+indir=${indir%/}
+total=$(ls $indir/*.vcf.gz| wc -l)
+
+echo "INPUT DIR $indir" | ts
+echo "PHENOTYPE FILE $phenofile" | ts
+echo "TOTAL FILES $total" | ts
+echo "CURRENT FILENO=$n" | ts
+
+if (( $n > $total ));then
+    echo "ERROR: there are $total < $n VCF files in $indir" | ts
+    exit 1
+fi
+
+fname=$(ls $indir/*.vcf.gz| sort | head -n $n | tail -n 1)
+outname=${fname/%.vcf.gz/.qctool.out}
+echo "CURRENT FILE $fname" | ts
+echo "OUTPUT FILE $outname" | ts
+echo "--------------------------------------------------------------"
+echo ""
+
+pheno_name=$(head -n 1 $phenofile| tr ' ' '\t' |cut -f 2)
+echo "INFO: phenotype name: $pheno_name"  | ts
+
+bcftools norm -m- "$fname" | bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%ALT' -Ov | qctool2 -g - -filetype vcf -differential "$pheno_name" -osnp "$outname" -s "$phenofile"
+echo "INFO: done" | ts
+
+exit 0
