43db6daac7de6eac072169269b9a3cd25336486b
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-22 18:01:07 +0100

    Create temp dir

--- collect_stats.sh
+++ collect_stats.sh
@@ -32,6 +32,13 @@ if [[ ! -d "$outdir" ]];then
     exit 1
 fi
 
+tempdir="$outdir"/temp
+mkdir -p "$tempdir"
+if [[ ! -d "$tempdir" ]];then
+    echo "ERROR: could not create temporary directory $tempdir; exit"
+    exit 1
+fi
+
 ID=$(sbatch --job-name=collect_stats --cpus-per-task=1 --mem-per-cpu=1G --time=10:00:00 -p normal_q --array="$chroms" -o "$logdir"/collect_stats_%A_chr_%a.log -e collect_stats_%A_chr_%a.err /compute/Genomics/software/scripts/general_purpose_scripts/collect_stats_chr.sh "$indir" "$outdir" "$pheno" | cut -d ' ' -f 4)
 
 # cleanup
