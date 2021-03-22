5d3bd106512aa75cd85970b62fd6d04a66d99aed
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-22 18:39:28 +0100

    Add command line options

--- collect_stats_chunk.sh
+++ collect_stats_chunk.sh
@@ -6,34 +6,50 @@
 
 set -eo pipefail
 
-indir=$1
-phenofile=$2
-n=$SLURM_ARRAY_TASK_ID
+function usage {
+    echo ""
+    echo "Usage: $0 -i <input dir>"
+    echo "          -p <pheno file>"
+    echo "          -f <file list>"
+    exit 0
+}
 
-indir=${indir%/}
-total=$(ls $indir/*.vcf.gz| wc -l)
+OPTIND=1
+while getopts "i:p:f:" optname; do
+    case "$optname" in
+        "i" ) input="${OPTARG}";;
+        "p" ) pheno="${OPTARG}";;
+        "f" ) flist="${OPTARG}";;
+        "?" ) usage ;;
+        *) usage ;;
+    esac;
+done
 
-echo "INPUT DIR $indir" | ts
-echo "PHENOTYPE FILE $phenofile" | ts
-echo "TOTAL FILES $total" | ts
-echo "CURRENT FILENO $n" | ts
-
-if (( $n > $total ));then
-    echo "ERROR: there are $total < $n VCF files in $indir" | ts
-    exit 1
+if [[ $# -eq 0 ]];then
+    usage
+    exit 0
 fi
 
-fname=$(ls $indir/*.vcf.gz| sort | head -n $n | tail -n 1)
+input=${input%/}
+
+# current file number in the file list
+n=$SLURM_ARRAY_TASK_ID
+
+fname=$(cat $flist | head -n $n | tail -n 1)
 outname=${fname/%.vcf.gz/.qctool.out}
+
+echo "INPUT DIR $input" | ts
+echo "PHENOTYPE FILE $pheno" | ts
+echo "CURRENT FILENO $n" | ts
 echo "CURRENT FILE $fname" | ts
 echo "OUTPUT FILE $outname" | ts
 echo "--------------------------------------------------------------"
 echo ""
 
-pheno_name=$(head -n 1 $phenofile| tr ' ' '\t' |cut -f 2)
+pheno_name=$(head -n 1 $pheno| tr ' ' '\t' |cut -f 2)
 echo "INFO: phenotype name: $pheno_name"  | ts
 
-bcftools norm -m- "$fname" | bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%ALT' -Ov | qctool2 -g - -filetype vcf -differential "$pheno_name" -osnp "$outname" -s "$phenofile"
+bcftools norm -m- "$fname" | bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%ALT' -Ov | qctool2 -g - -filetype vcf -differential "$pheno_name" -osnp "$outname" -s "$pheno"
 
 if [[ $? -eq 0 ]];then
     echo "INFO: removing $fname" | ts
