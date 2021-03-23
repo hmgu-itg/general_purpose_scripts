272b8bd717e1fdbbae3d35b336af0cef9d54d6fc
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-23 12:55:01 +0100

    Finalize filter_chr.sh

--- filter_chr.sh
+++ filter_chr.sh
@@ -5,17 +5,23 @@ function usage {
     echo "Usage: $0 -i <input dir>"
     echo "          -o <output dir>"
     echo "          -p <pheno file>"
+    echo "        { -m : <mode>; optional, \"stats\" or \"full\"; default: \"full\" }"
+    echo "        { -t : <pvalue threshold>; only required if mode is \"full\"}"
     echo "        { -r : if analyses should be reusmed; optional; default: false }"
     exit 0
 }
 
 resume="no"
+mode="full"
+pt=""
 OPTIND=1
-while getopts "i:o:p:r" optname; do
+while getopts "i:o:p:m:t:r" optname; do
     case "$optname" in
         "i" ) input="${OPTARG}";;
         "o" ) output="${OPTARG}";;
         "p" ) pheno="${OPTARG}";;
+        "m" ) mode="${OPTARG}";;
+        "t" ) pt="${OPTARG}";;
         "r" ) resume="yes";;
         "?" ) usage ;;
         *) usage ;;
@@ -23,8 +29,19 @@ while getopts "i:o:p:r" optname; do
 done
 
 if [[ $# -eq 0 ]];then
+    echo "No arguments provided; exit"
     usage
     exit 0
+fi
+
+if [[ "$mode" != "full" && "$mode" != "stats" ]];then
+    echo "ERROR: mode (-m) should be either \"full\" or \"stats\"; exit"
+    exit 1
+fi
+
+if [[ "$mode" == "full" && -z "$pt" ]];then
+    echo "ERROR: no pvalue threshold (-t) specified; exit"
+    exit 1
 fi
 
 input=${input%/}
@@ -36,6 +53,8 @@ c=$SLURM_ARRAY_TASK_ID
 echo "INPUT DIR $input"
 echo "ROOT OUTPUT DIR $output"
 echo "PHENOTYPE FILE $pheno"
+echo "MODE $mode"
+echo "PVALUE THRESHOLD $pt"
 echo "RESUME $resume"
 echo "CURRENT CHR $c"
 
@@ -44,17 +63,17 @@ logdir="$output"/logs
 tempdir="$output"/temp
 
 if [[ ! -d "$logdir" ]];then
-    echo "ERROR: log dir $logdir does not exist"
+    echo "ERROR: log dir $logdir does not exist; exit"
     exit 1
 fi
 
 if [[ ! -d "$tempdir" ]];then
-    echo "ERROR: temp dir $tempdir does not exist"
+    echo "ERROR: temp dir $tempdir does not exist; exit"
     exit 1
 fi
 
 if [[ ! -d "$output" ]];then
-    echo "ERROR: root output dir $output does not exist"
+    echo "ERROR: root output dir $output does not exist; exit"
     exit 1
 fi
 
@@ -62,7 +81,7 @@ fi
 output2="$output"/chr"$c"
 mkdir -p "$output2"
 if [[ ! -d "$output2" ]];then
-    echo "ERROR: could not create output directory $output2 for chr $c"
+    echo "ERROR: could not create output directory $output2 for chr $c; exit"
     exit 1
 fi
 
@@ -84,18 +103,24 @@ if [[ "$resume" == "no" ]];then
     done
 fi
 
-for f in $(find "${output2}" -mindepth 1 -maxdepth 1 -name "*.vcf.gz");do
+# exclude "filtered" output VCFs
+for f in $(find "${output2}" -mindepth 1 -maxdepth 1 -name "*.vcf.gz" ! -name "*.filtered.vcf.gz");do
     echo "$f" >> "$flist"
 done
 
 total=$(cat $flist | wc -l)
 
 if [[ "$total" -eq 0 ]];then
-    echo "INFO: no VCFs for chr $c found in $input; exit"
+    echo "INFO: no input VCFs for chr $c found in $output2; exit"
     exit 0
 fi
 
-ID=$(sbatch --job-name=collect_stats_chr_"$c" --cpus-per-task=1 --mem-per-cpu=10G --time=10:00:00 -p normal_q --array=1-"$total" -o "$logdir"/collect_stats_chr_"$c"_%A_part_%a.log -e "$logdir"/collect_stats_chr_"$c"_%A_part_%a.err --wrap="singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 /compute/Genomics/software/scripts/general_purpose_scripts/collect_stats_chunk.sh -i $output2 -p $pheno -f $flist" | cut -d ' ' -f 4)
+topt=""
+if [[ ! -z "$pt" ]];then
+    topt="-t $pt"
+fi
+
+ID=$(sbatch --job-name=filter_chr_"$c" --cpus-per-task=1 --mem-per-cpu=10G --time=10:00:00 -p normal_q --array=1-"$total" -o "$logdir"/filter_chr_"$c"_%A_part_%a.log -e "$logdir"/filter_chr_"$c"_%A_part_%a.err --wrap="singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 /compute/Genomics/software/scripts/general_purpose_scripts/filter_chunk.sh -p $pheno -f $flist" -m "$mode" "$topt" | cut -d ' ' -f 4)
 
 # remove file list after chr processing is done
 
