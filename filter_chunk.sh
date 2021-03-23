9da4663a221a8208535a882c56b2527b2226fb6d
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-23 12:39:46 +0100

    Finalize filter_dir.sh

---
+++ filter_chunk.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+#
+# using bcftools and qctool2 to collect missingness stats from one VCF file
+#
+###################
+
+set -eo pipefail
+
+function usage {
+    echo ""
+    echo "Usage: $0 -i <input dir>"
+    echo "          -p <pheno file>"
+    echo "          -f <file list>"
+    exit 0
+}
+
+OPTIND=1
+while getopts "i:p:f:" optname; do
+    case "$optname" in
+        "i" ) input="${OPTARG}";;
+        "p" ) pheno="${OPTARG}";;
+        "f" ) flist="${OPTARG}";;
+        "?" ) usage ;;
+        *) usage ;;
+    esac;
+done
+
+if [[ $# -eq 0 ]];then
+    usage
+    exit 0
+fi
+
+input=${input%/}
+
+if [[ ! -f "$flist" ]];then
+    echo "ERROR: file list $flist does not exist; exit"
+    exit 1
+fi
+
+# current file number in the file list
+n=$SLURM_ARRAY_TASK_ID
+
+fname=$(cat $flist | head -n $n | tail -n 1)
+outname=${fname/%.vcf.gz/.qctool.out}
+
+echo "INPUT DIR $input" | ts
+echo "PHENOTYPE FILE $pheno" | ts
+echo "CURRENT FILENO $n" | ts
+echo "CURRENT FILE $fname" | ts
+echo "OUTPUT FILE $outname" | ts
+echo "--------------------------------------------------------------"
+echo ""
+
+pheno_name=$(head -n 1 $pheno| tr ' ' '\t' |cut -f 2)
+echo "INFO: phenotype name: $pheno_name"  | ts
+
+bcftools norm -m- "$fname" | bcftools annotate --set-id +'%CHROM\_%POS\_%REF\_%ALT' -Ov | qctool2 -g - -filetype vcf -differential "$pheno_name" -osnp "$outname" -s "$pheno"
+
+if [[ $? -eq 0 ]];then
+    echo "INFO: removing $fname" | ts
+    rm -v "$fname"
+else
+    echo "INFO: something went wrong; keeping $fname" | ts
+fi
+
+echo "INFO: done" | ts
+echo "--------------------------------------------------------------"
+echo ""
+
+exit 0
---
+++ filter_dir.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#
+# top level script for processing VCF chunk files
+#
+########################
+
+function usage {
+    echo ""
+    echo "Usage: $0"
+    echo "          -i <input dir>"
+    echo "          -o <output dir>"
+    echo "          -p <pheno file>"
+    echo "        { -c <chromosome(s)> : optional; default: 1-22 }"
+    echo "        { -m : <mode>; optional, \"stats\" or \"full\"; default: \"full\" }"
+    echo "        { -t : <pvalue threshold>; only required if mode is \"full\"}"
+    echo "        { -r : if analyses should be rerun; optional; default: false }"
+    exit 0
+}
+
+resume="no"
+chroms="1-22"
+mode="full"
+pt=""
+OPTIND=1
+while getopts "i:o:p:c:m:t:r" optname; do
+    case "$optname" in
+        "i" ) input="${OPTARG}";;
+        "o" ) output="${OPTARG}";;
+        "p" ) pheno="${OPTARG}";;
+        "c" ) chroms="${OPTARG}";;
+        "m" ) mode="${OPTARG}";;
+        "t" ) pt="${OPTARG}";;
+        "r" ) resume="yes";;
+        "?" ) usage ;;
+        *) usage ;;
+    esac;
+done
+
+if [[ $# -eq 0 ]];then
+    echo "No arguments provided; exit"
+    usage
+    exit 0
+fi
+
+if [[ "$mode" != "full" && "$mode" != "stats" ]];then
+    echo "ERROR: mode (-m) should be either \"full\" or \"stats\"; exit"
+    exit 1
+fi
+
+if [[ "$mode" == "full" && -z "$pt" ]];then
+    echo "ERROR: no pvalue threshold (-t) specified; exit"
+    exit 1
+fi
+
+input=${input%/}
+output=${output%/}
+
+echo "INPUT DIR $input"
+echo "OUTPUT DIR $output"
+echo "PHENOTYPE FILE $pheno"
+echo "MODE $mode"
+echo "PVALUE THRESHOLD $pt"
+echo "CHROMS $chroms"
+echo "RESUME $resume"
+echo "--------------------------------------"
+echo ""
+
+logdir="$output"/logs
+mkdir -p "$logdir"
+if [[ ! -d "$logdir" ]];then
+    echo "ERROR: could not create log directory $logdir; exit"
+    exit 1
+fi
+
+tempdir="$output"/temp
+mkdir -p "$tempdir"
+if [[ ! -d "$tempdir" ]];then
+    echo "ERROR: could not create temporary directory $tempdir; exit"
+    exit 1
+fi
+
+resopt=""
+if [[ "$resume" == "yes" ]];then
+    resopt="-r"
+fi
+
+topt=""
+if [[ ! -z "$pt" ]];then
+    topt="-t $pt"
+fi
+
+sbatch --job-name=collect_stats --cpus-per-task=1 --mem-per-cpu=1G --time=10:00:00 -p normal_q --array="$chroms" -o "$logdir"/collect_stats_%A_chr_%a.log -e "$logdir"/collect_stats_%A_chr_%a.err /compute/Genomics/software/scripts/general_purpose_scripts/filter_chr.sh -i "$input" -o "$output" -p "$pheno" -m "$mode" "$topt" "$resopt"
