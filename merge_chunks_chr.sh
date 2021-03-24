a93bc635fb3b29ac98f1738006cbfe25cdb1c24f
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-24 19:04:39 +0100

    Update merging script

--- merge_chunks_chr.sh
+++ merge_chunks_chr.sh
@@ -2,10 +2,11 @@
 
 indir=$1
 threads=$2
+c=$3
 
 indir=${indir%/}
-output="$indir"/merged.vcf.gz
-plout="$indir"/merged
+output="$indir"/merged_chr"$c".vcf.gz
+plout="$indir"/merged_chr"$c"
 
 echo "MERGING VCF FILES AND CONVERTING TO PLINK FORMAT"
 echo "INPUT DIR $indir"
@@ -16,25 +17,65 @@ echo "START" $(date)
 echo "-------------------------------------"
 echo ""
 
-flist=$(mktemp -t merge_list-XXXXXXX)
+flist=$(mktemp -t merge_list_chr"$c"-XXXXXXX)
 if [[ ! -f $flist ]];then
-    echo "ERROR: could not create merging list file; exit"
+    echo "ERROR: could not create list file; exit"
     exit 1
 fi
 
-for f in $(find "$indir" -maxdepth 1 -mindepth 1 -name "*.vcf.gz");do
+for f in $(find "$indir" -maxdepth 1 -mindepth 1 -name "*.vcf.gz" ! -name "merged*.vcf.gz");do
     realpath $f >> "$flist"
 done
 
-echo "MERGING FILES:"
-cat "$flist"
-echo ""
+# bcftools merge
+if [[ ! -s "$output" ]];then
+    echo $(date) "MERGING FILES:"
+    cat "$flist"
+    echo ""
+    singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -a -f "$flist" -Oz -o "$output" --threads "$threads"
+    if [[ $? -ne 0 ]];then
+	echo "ERROR: merging failed; exit"
+	if [[ -f "$output" ]];then rm -v "$output";fi
+	rm -v "$flist"
+	exit 1
+    fi
+    echo "MERGING DONE" $(date)
+else
+    echo "INFO: merged VCF $output already exists; skipping merging"
+fi
 
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 bcftools concat -a -f "$flist" -Oz -o "$output" --threads "$threads"
-echo "MERGING DONE" $(date)
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 tabix "$output"
-echo "INDEXING DONE" $(date)
-rm -f "$flist"
+# tabix
+if [[ ! -s "$output".tbi ]];then
+    singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 tabix "$output"
+    if [[ $? -ne 0 ]];then
+	echo "ERROR: tabix failed; exit"
+	if [[ -f "$output" ]];then rm -v "$output";fi
+	if [[ -f "$output".tbi ]];then rm -v "$output".tbi;fi
+	rm -v "$flist"
+	exit 1
+    fi
+    echo "INDEXING DONE" $(date)
+else
+    echo "INFO: index file ${output}.tbi already exists; skipping indexing"
+fi
 
-# TODO: change --vcf-half-call mode if necessary
-singularity exec -B /compute/Genomics /compute/Genomics/containers/worker_3.1 plink2 --vcf "$output" --make-pgen --out "$plout" --threads "$threads" --vcf-half-call m
+# PLINK
+flist2=$(mktemp -t plink_list_chr"$c"-XXXXXXX)
+if [[ ! -f $flist2 ]];then
+    echo "ERROR: could not create PLINK2 merging list file; exit"
+    exit 1
+fi
+
+cat "$flist" | while read fname
+do
+    outname=${fname/%.vcf.gz}
+    /compute/Genomics/software/plink2/plink2_23.Mar.2021/plink2 --vcf "$fname" --make-pgen --out "$outname" --threads "$threads" --vcf-half-call m  
+    echo "$outname".pgen "$outname".pvar "$outname".psam >> "$flist2"
+done
+
+/compute/Genomics/software/plink2/plink2_23.Mar.2021/plink2 --pmerge-list "$flist2" --make-pgen --out "$plout" --threads "$threads"
+
+rm -v "$flist"
+rm -v "$flist2"
+
+echo "INFO: done" $(date)
