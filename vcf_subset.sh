dd261e6c96fd73463c843c6c6eac5994d3121df1
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-25 12:27:33 +0100

    Minor fix

--- vcf_subset.sh
+++ vcf_subset.sh
@@ -7,7 +7,7 @@ header=$(mktemp -t header-XXXXXXX)
 body=$(mktemp -t body-XXXXXXX)
 
 bcftools view -h "$infile" -Ov > "$header"
-bcftools view -H "$infile" -Ov | perl -lne 'BEGIN{$n=0;}{$x=rand();if ($x<0.1){print $_;$n++;} exit 0 if $n>100;}' > "$body"
+bcftools view -H "$infile" -Ov | perl -lne 'BEGIN{$n=0;}{$x=rand();if ($x<0.1){print $_;$n++;} exit 0 if $n>1000;}' > "$body"
 cat "$header" "$body" | bgzip - > "$outfile"
 tabix -p vcf "$outfile"
 rm -v "$header" "$body"
