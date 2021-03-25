6bc4f6d900a6d5e3c78ac218edac087f765567da
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-25 12:38:47 +0100

    Minor fix

--- vcf_subset.sh
+++ vcf_subset.sh
@@ -3,7 +3,7 @@
 infile=$1
 outfile=$1
 
-tfile=$(mktemp -t header-XXXXXXX)
+tfile=$(mktemp -t vcf-XXXXXXX)
 
 bcftools view -h "$infile" -Ov > "$tfile"
 bcftools view -H "$infile" -Ov | perl -lne 'BEGIN{$n=0;}{$x=rand();if ($x<0.1){print $_;$n++;} exit 0 if $n>1000;}' >> "$tfile"
