083370ab3cc2ee2e3530bab6e8b315df7a682682
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-03-25 13:08:41 +0100

    Minor fix

--- vcf_subset.sh
+++ vcf_subset.sh
@@ -3,10 +3,9 @@
 infile=$1
 outfile=$2
 
-tfile=$(mktemp -t vcf-XXXXXXX)
+tfile=${outfile/%.gz}
 
 bcftools view -h "$infile" -Ov > "$tfile"
 bcftools view -H "$infile" -Ov | perl -lne 'BEGIN{$n=0;}{$x=rand();if ($x<0.1){print $_;$n++;} exit 0 if $n>1000;}' >> "$tfile"
-bgzip "$tfile" > "$outfile"
+bgzip "$tfile"
 tabix -p vcf "$outfile"
-rm "$tfile"
