6ce0ea9c1fab8d1d03bf7bfd4a119474014f96e7
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-06-18 13:02:58 +0200

    Minor bug fixes

--- ukbb/generate_input.sh
+++ ukbb/generate_input.sh
@@ -17,10 +17,9 @@ prefix=$4
 
 m=$((cols*N))
 n=${#m}
-   
+declare -a rows
+mr=0
 for i in $(seq -w 1 $N);do
-    echo -n "Generating file $i/$N" 1>&2
-    fname=${prefix}_${i}".txt.gz"
     del=$((RANDOM%2))
     # increase/decrease by up to 5%
     max_mod=$(echo $rows/20|bc)
@@ -29,12 +28,23 @@ for i in $(seq -w 1 $N);do
 	to_mod="-"$to_mod
     fi
     r=$((rows+to_mod))
+    rows+=($r)
+    if [[ $r -gt $mr ]];then
+	mr=$r
+    fi
+done
+
+   
+for i in $(seq -w 1 $N);do
+    echo -n "Generating file $i/$N" 1>&2
+    fname=${prefix}_${i}".txt.gz"
+    r=${rows[$i]}
     echo " ($r rows)"
     
     echo -ne "f.eid\t" | gzip -c > $fname
     for j in $(seq $(((i-1)*cols+1)) $((i*cols)));do echo $j;done|perl -slne 'BEGIN{@a=();}{push @a,sprintf("COL%0".$w."d",$_);}END{print join("\t",@a);}' -- -w=$n | gzip -c >> $fname
-    for j in $(seq -w 1 $r | shuf);do
+    for j in $(seq -w 1 $mr);do
 	echo -ne "$j\t"
 	tr -dc 0-9 </dev/urandom | head -c $(echo "6*$cols" | bc -l)|perl -lne 'print join("\t",/(.{1,6})/g);'|gawk -v seed=$RANDOM 'BEGIN{FS=OFS="\t";srand(seed);}{x=rand();n=1+int(NF*rand());if (x>=0 && x<0.05){$n="";} if (x>=0.05 && x<0.1){$n="\""$n"\"";} if(x>=0.1 && x<0.15){$n=rand();} if(x>=0.15 && x<0.2){$n=rand()", "rand();} if(x>=0.2 && x<0.25){$n="\""rand()", "rand()"\"";} if(x>=0.25 && x<0.3){$n=" ";} print $0;}'
-    done | gzip - -c >> $fname
+    done | head -n $r | gzip - -c >> $fname
 done
