fde360dc6b2bf3be1daaee8943090fb5af6a4f1a
git <andrei.barysenka@helmholtz-muenchen.de>
2021-07-12 23:51:15 +0200

    Finalize ukbb_release_info.sh

--- ukbb/ukbb_release_info.sh
+++ ukbb/ukbb_release_info.sh
@@ -8,10 +8,6 @@ function usage () {
     exit 0
 }
 
-if [[ $# -eq 0 ]];then
-    usage
-fi
-
 scriptname=$0
 scriptdir=$(dirname $(readlink -f $0))
 source "${scriptdir}/functions.sh"
@@ -29,9 +25,27 @@ while getopts "hc:" opt; do
 done
 
 if [[ -z "$config" ]];then
-    encoding="${scriptdir}"/config.txt
-    echo "INFO: no config file specified, trying to use $config"
+    config="${scriptdir}"/config.txt
+    echo "INFO: no config file specified, using $config"
 fi
 exitIfNotFile "$config" "ERROR: config $config does not exist"
 readAArray "$config" PROJECTS projects
 readValue "$config" DATA_PATH data_path
+echo -e "PROJECT\tDATASET\tRELEASE\tCREATED\tFILE"
+for p in "${!projects[@]}";do
+    dir="$data_path"/"${projects[$p]}"/"releases"
+    for f in $(find "$dir" -maxdepth 1 -name "phenotypes_r*.txt.gz"| sort);do
+	rc=$(getColNum "$f" "RELEASE" "zcat")
+	rel=$(zcat "$f"|head -n 3|tail -n 1|cut -f $rc)
+	cc=$(getColNum "$f" "CREATED" "zcat")
+	cr=$(zcat "$f"|head -n 3|tail -n 1|cut -f $cc)
+	echo -e "$p\tmain\t$rel\t$cr\t$f"
+    done
+    for f in $(find "$dir" -maxdepth 1 -name "hesin_r*.tar.gz"| sort);do
+	rel=$(tar -zxf "$f" RELEASE -O)
+	cr=$(tar -zxf "$f" CREATED -O)
+	echo -e "$p\thesin\t$rel\t$cr\t$f"
+    done
+done
+
+exit 0
