7092bde4015fb69372a4f42bc86b7f6824b92b2a
git <andrei.barysenka@helmholtz-muenchen.de>
2021-08-03 00:44:51 +0200

    Use strict, warnings

--- ukbb/removeEmpty.pl
+++ ukbb/removeEmpty.pl
@@ -1,9 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+use strict;
 
 $\="\n";
 while(<STDIN>){
     chomp;
-    @a=split(/\t/,$_,-1);
-    for ($i=0;$i<scalar(@a);$i++){$a[$i]="NA" if length($a[$i])==0;}
+    my @a=split(/\t/,$_,-1);
+    for (my $i=0;$i<scalar(@a);$i++){$a[$i]="NA" if length($a[$i])==0;}
     print join("\t",@a);
 }
