873b000dc31c5d3d9e2d15742ae832d0c18fe266
git <andrei.barysenka@helmholtz-muenchen.de>
2021-09-23 06:38:00 +0000

    Add ICD exclusion to OA_cases_SR.sh

--- ukbb/OA_cases_SR.sh
+++ ukbb/OA_cases_SR.sh
@@ -64,8 +64,52 @@ exitIfExists "$outfile" "ERROR: output file $outfile already exists"
 logfile="${outprefix}".log
 : > "${logfile}"
 
-join -1 1 -2 1 -a 1 -t$'\t' -e NULL -o 1.1,2.1 <("$script" -p "OA" -r "$release" --cc 20002,1465 -c "$config" 2>"$logfile" | tail -n +2 | grep "1$" | cut -f 1) <("$hesin_script" -p "OA" -r "$hesin_release" --icd9 <(grep ^icd9 "$icd_exclusion_file"| cut -f 2) --icd10 <(grep ^icd10 "$icd_exclusion_file"| cut -f 2) 2>/dev/null) | grep NULL | cut -f 1 >"$outfile"
+# create temp files
+out_dir=$(dirname "$outfile")
+tmp_icd9=$(mktemp -p "$out_dir" temp_icd9_XXXXXXXX)
+if [[ $? -ne 0 ]];then
+    echo "ERROR: could not create temp ICD9 file"|tee -a "$logfile"
+    exit 1
+fi
 
-# "$script" -p "OA" -r "$release" --cc 20002,1465 -c "$config" 2>"$logfile" | tail -n +2 | grep "1$" | cut -f 1 >"$outfile" 
+tmp_icd10=$(mktemp -p "$out_dir" temp_icd10_XXXXXXXX)
+if [[ $? -ne 0 ]];then
+    echo "ERROR: could not create temp ICD10 file"|tee -a "$logfile"
+    rm -f "$tmp_icd9"
+    exit 1
+fi
+
+tmp_icd_excl=$(mktemp -p "$out_dir" temp_icd_excl_XXXXXXXX)
+if [[ $? -ne 0 ]];then
+    echo "ERROR: could not create temp ICD exclusion file"|tee -a "$logfile"
+    rm -f "$tmp_icd9"
+    rm -f "$tmp_icd10"
+    exit 1
+fi
+
+tmp_sr_oa=$(mktemp -p "$out_dir" temp_sr_oa_XXXXXXXX)
+if [[ $? -ne 0 ]];then
+    echo "ERROR: could not create temp SR OA inclusion file"|tee -a "$logfile"
+    rm -f "$tmp_icd9"
+    rm -f "$tmp_icd10"
+    rm -f "$tmp_icd_excl"
+    exit 1
+fi
+
+# save exclusion ICD codes
+grep ^icd9 "$icd_exclusion_file"| cut -f 2 > "$tmp_icd9"
+grep ^icd10 "$icd_exclusion_file"| cut -f 2 > "$tmp_icd10"
+
+# save IDs having exclusion ICD codes
+"$hesin_script" -p "OA" -r "$hesin_release" --icd9 "$tmp_icd9" --icd10 "$tmp_icd10" 2>>"$logfile" | sort >"$tmp_icd_excl" 
+
+# save self-reported OA cases
+"$script" -p "OA" -r "$release" --cc 20002,1465 -c "$config" 2>>"$logfile" | tail -n +2 | grep "1$" | cut -f 1 | sort >"$tmp_sr_oa"
+
+# output self-reported OA cases without exclusion ICD codes
+join -1 1 -2 1 -a 1 -t$'\t' -e NULL -o 1.1,2.1 "$tmp_sr_oa" "$tmp_icd_excl" | grep NULL | cut -f 1 >"$outfile"
+
+# delete temp files
+rm -f "$tmp_icd9" "$tmp_icd10" "$tmp_icd_excl" "$tmp_sr_oa"
 
 exit 0
