5bbc5ff03f35bb3b2f3ff9cf1bff34b5a2003eb9
git <andrei.barysenka@helmholtz-muenchen.de>
2021-09-24 14:53:46 +0000

    addSummaryColumn

--- ukbb/python/itgukbb/utils.py
+++ ukbb/python/itgukbb/utils.py
@@ -24,3 +24,56 @@ def readDataDictionary(fname):
     else:
         print("ERROR: dictionary file %s does not exist" % fname,file=sys.stderr)
         return None
+
+def customMean(L):
+    s=0.0
+    c=0
+    for x in L:
+        if x!="NA":
+            s+=x
+            c+=1
+    if c==0:
+        return "NA"
+    else:
+        return s/c
+
+def customMajority(L):
+    H=dict()
+    f=1
+    for x in L:
+        if x!="NA":
+            f=0
+            H[x]=H.setdefault(x,0)+1
+    if f==1:
+        return "NA"
+    else:
+        return max(H,key=H.get)
+
+def customCC(L,values):
+    f=1
+    for x in L:
+        if x!="NA":
+            f=0
+            if x in values:
+                return 1
+    if f==1:
+        return "NA"
+    else:
+        return 0
+
+def addSummaryColumn(df,columns,new_name,values,method):
+    if method=="mean":
+        df[new_name]=df[columns].agg(customMean,axis="columns")
+    elif method=="majority":
+        df[new_name]=df[columns].agg(customMajority,axis="columns")
+    elif method=="cc":
+        df[new_name]=df[columns].agg(lambda L:customCC(L,values),axis="columns")
+    elif method=="minmissing":
+        H=dict()
+        for c in columns:
+            H[c]=(df[c]=="NA").sum()
+        c0=min(H,key=H.get)
+        df[new_name]=df[c0]
+    else:
+        print("ERROR: unknown method %s" % (method))
+    return df
