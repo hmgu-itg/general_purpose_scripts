3f7c289297e5a95eaaa6a66c69315590f6b22c04
andrei-barysenka <andrei.barysenka@gmail.com>
2022-09-19 08:11:53 +0000

    finalize finemap_prepare

--- finemap_prepare.sh
+++ finemap_prepare.sh
@@ -69,8 +69,6 @@ exitUnlessExists "${panel_prefix}".bim "ERROR: ${panel_prefix}.bim does not exis
 exitUnlessExists "${panel_prefix}".bed "ERROR: ${panel_prefix}.bed does not exist"
 exitUnlessExists "${panel_prefix}".fam "ERROR: ${panel_prefix}.fam does not exist"
 
-# sep=" "
-
 getColNumbersSep "$input_fname" "cat" " " input_colnames
 
 # for i in "${!input_colnames[@]}"
@@ -81,6 +79,22 @@ getColNumbersSep "$input_fname" "cat" " " input_colnames
 
 # get column numbers
 
+for n in "chromosome" "position" "rsid" "allele1" "allele2" "beta" "se" "n" "maf";do
+    if [[ -v "input_colnames[$n]" ]] ; then
+	eval "${n}_column=${input_colnames[$n]}"
+    else
+	echo "ERROR: could not find $n in the input header"
+	exit 1
+    fi
+done
+
+# for n in "chromosome" "position" "rsid" "allele1" "allele2" "beta" "se" "n" "maf";do
+#     v="${n}_column"
+#     echo "$n : ${!v}"
+# done
+
+# exit 0
+
 if [[ -v "input_colnames[$eff_colname]" ]] ; then
     eff_column=${input_colnames[$eff_colname]}
 else
@@ -88,73 +102,8 @@ else
     exit 1
 fi
 
-echo "$eff_colname --> $eff_column"
-
-if [[ -v "input_colnames[chromosome]" ]] ; then
-    chr_column=${input_colnames["chromosome"]}
-else
-    echo "ERROR: could not find \"chromosome\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[position]" ]] ; then
-    pos_column=${input_colnames["position"]}
-else
-    echo "ERROR: could not find \"position\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[rsid]" ]] ; then
-    id_column=${input_colnames["rsid"]}
-else
-    echo "ERROR: could not find \"rsid\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[allele1]" ]] ; then
-    a1_column=${input_colnames["allele1"]}
-else
-    echo "ERROR: could not find \"allele1\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[allele2]" ]] ; then
-    a2_column=${input_colnames["allele2"]}
-else
-    echo "ERROR: could not find \"allele2\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[maf]" ]] ; then
-    maf_column=${input_colnames["maf"]}
-else
-    echo "ERROR: could not find \"maf\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[beta]" ]] ; then
-    beta_column=${input_colnames["beta"]}
-else
-    echo "ERROR: could not find \"beta\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[se]" ]] ; then
-    se_column=${input_colnames["se"]}
-else
-    echo "ERROR: could not find \"se\" in the input header"
-    exit 1
-fi
-
-if [[ -v "input_colnames[n]" ]] ; then
-    n_column=${input_colnames["n"]}
-else
-    echo "ERROR: could not find \"n\" in the input header"
-    exit 1
-fi
-
 # check if input has any duplicate positions
-ndup=$(tail -n +2 $input_fname| cut -d ' ' -f $chr_column,$pos_column| tr ' ' ':'| sort|uniq -d| wc -l)
+ndup=$(tail -n +2 $input_fname| cut -d ' ' -f $chromosome_column,$position_column| tr ' ' ':'| sort|uniq -d| wc -l)
 
 if [[ $ndup -ne "0" ]];then
     echo "ERROR: there are chr:pos duplicates in input"
@@ -165,39 +114,29 @@ fi
 while read id;do
     id_mapping[$id]=$id
     id_mapping[$(switchIdAlleles $id)]=$id
-done < <(tail -n +2 $input_fname| cut -d ' ' -f $id_column)
+done < <(tail -n +2 $input_fname| cut -d ' ' -f $rsid_column)
 
 # create "old ID" --> "ref allele" mapping
-if [[ "$id_column" -lt "$eff_column" ]];then
+if [[ "$rsid_column" -lt "$eff_column" ]];then
     while read id a;do
 	ref_alleles[$id]=$a
-    done < <(tail -n +2 $input_fname| cut -d ' ' -f $id_column,$eff_column)
+    done < <(tail -n +2 $input_fname| cut -d ' ' -f $rsid_column,$eff_column)
 else
     while read a id;do
 	ref_alleles[$id]=$a
-    done < <(tail -n +2 $input_fname| cut -d ' ' -f $id_column,$eff_column)
+    done < <(tail -n +2 $input_fname| cut -d ' ' -f $rsid_column,$eff_column)
 fi
 
-id="4:1234567_A_G"
-newID=$(switchIdAlleles $id)
-echo $id
-echo $newID
-
 # create temp dir
-tmpdir=$(mktemp -d -p $(dirname $(realpath $output_prefix)) ${output_prefix}_XXXXXXXX)
+bname=$(basename $(realpath $output_prefix))
+tmpdir=$(mktemp -d -p $(dirname $(realpath $output_prefix)) ${bname}_XXXXXXXX)
 echo "DEBUG: created $tmpdir"
-
-# for i in "${!ref_alleles[@]}"
-# do
-#   echo "$i" "${ref_alleles[$i]}"
-# done
 
 # effect allele as reference allele
 ref_file=${tmpdir}/ref
 for id in "${!id_mapping[@]}"
 do
     oldID=${id_mapping[${id}]}
-    # echo $id $oldID
     a=${ref_alleles[$oldID]}
     echo $id $a
 done > ${ref_file}
@@ -208,6 +147,8 @@ for i in "${!id_mapping[@]}"
 do
   echo "$i"
 done > ${ex_file}
+
+echo "Extracting $(cat ${ex_file}| wc -l) variants"
 
 # calling PLINK
 res=${tmpdir}/plink_output
@@ -222,8 +163,7 @@ output_z="${output_prefix}".z
 echo  "rsid chromosome position allele1 allele2 maf beta se" > "$output_z"
 while read id;do
     oldID=${id_mapping[$id]}
-    #echo "id=$oldID rs=$id_column chr=$chr_column pos=$pos_column a1=$a1_column a2=$a2_column maf=$maf_column beta=$beta_column se=$se_column"
-    awk -v x="$oldID" -v r="$id_column" -v c="$chr_column" -v p="$pos_column" -v a="$a1_column" -v c="$a2_column" -v m="$maf_column" -v b="$beta_column" -v s="$se_column" '{if ($r==x){print x,$c,$p,$a,$c,$m,$b,$s;}}' "$input_fname" >>  "$output_z"
+    awk -v x="$oldID" -v r="$rsid_column" -v c="$chromosome_column" -v p="$position_column" -v a="$allele1_column" -v c="$allele2_column" -v m="$maf_column" -v b="$beta_column" -v s="$se_column" '{if ($r==x){print x,$c,$p,$a,$c,$m,$b,$s;}}' "$input_fname" >>  "$output_z"
 done < <(cat ${res}.snplist)
 
 # N samples for the master file: the max number of samples in the input file
