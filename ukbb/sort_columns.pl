1c35b94b81c8832fb1c6f935abc3c2c5626972e1
andrei-barysenka <andrei.barysenka@gmail.com>
2023-09-30 05:00:17 +0200

    bug fix

--- ukbb/sort_columns.pl
+++ ukbb/sort_columns.pl
@@ -11,19 +11,19 @@ while(<STDIN>){
     my @a=split(/\t/,$_,-1);
     if ($nr == 0){
 	push @S,$a[0];
-	for (my $i=0;$i<scalar(@a);$i++){
-	    $name2i{$a[$i]}=$i;
+	for (my $i=1;$i<scalar(@a);$i++){
+	    push @{$name2i{$a[$i]}},$i;
 	}
 	foreach my $c (sort keys %name2i){
-	    if ($c ne $S[0]){
-		push @S,$c;
-	    }
+	    push @S,$c;
+	    push @S,$c;
 	}
 	print STDOUT join("\t",@S);
     }else{
 	my @L=();
 	foreach my $c (@S){
-	    push @L,$a[$name2i{$c}];
+	    push @L,$a[${$name2i{$c}}[0]];
+	    push @L,$a[${$name2i{$c}}[1]];
 	}
 	print STDOUT join("\t",@L);
     }
