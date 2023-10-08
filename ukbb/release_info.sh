88ea9d728d3f6443384bd980daa6c2c650924c70
andrei-barysenka <andrei.barysenka@gmail.com>
2023-10-08 20:53:09 +0200

    update

--- ukbb/release_info.sh
+++ ukbb/release_info.sh
@@ -31,20 +31,21 @@ fi
 exitIfNotFile "$config" "ERROR: config $config does not exist"
 readAArray "$config" PROJECTS projects
 readValue "$config" DATA_PATH data_path
-echo -e "PROJECT\tDATASET\tRELEASE\tCREATED\tFILE"
+echo -e "PROJECT\tDATASET\tRELEASE\tCREATED\tSAMPLES\tFIELDS"
 for p in "${!projects[@]}";do
     dir="$data_path"/"${projects[$p]}"/"releases"
     for f in $(find "$dir" -maxdepth 1 -name "phenotypes_r*.txt.gz"| sort);do
-	rc=$(getColNum "$f" "RELEASE" "zcat")
-	rel=$(zcat "$f"|head -n 3|tail -n 1|cut -f $rc)
-	cc=$(getColNum "$f" "CREATED" "zcat")
-	cr=$(zcat "$f"|head -n 3|tail -n 1|cut -f $cc)
-	echo -e "$p\tmain\t$rel\t$cr\t$f"
+	rel=$(getRelease "$f" "zcat")
+	cr=$(getCreated "$f" "zcat")
+	samples=$(zcat "$f" | tail -n +2 | wc -l)
+	fields=$(zcat "$f" | head -n 1 | tr '\t' '\n' | wc -l)
+	fields=$((fields-3)) # exclude f.eid, RELEASE, CREATED
+	echo -e "${p}\tmain\t${rel}\t${cr}\t${samples}\t${fields}"
     done
     for f in $(find "$dir" -maxdepth 1 -name "hesin_r*.tar.gz"| sort);do
 	rel=$(tar -zxf "$f" RELEASE -O)
 	cr=$(tar -zxf "$f" CREATED -O)
-	echo -e "$p\thesin\t$rel\t$cr\t$f"
+	echo -e "${p}\thesin\t${rel}\t${cr}\tNA\tNA"
     done
 done
 
