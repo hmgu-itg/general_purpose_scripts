91b1acb0ec57c579240c2606c192fc5dd0b41bfd
andrei-barysenka <andrei.barysenka@gmail.com>
2023-10-08 05:32:21 +0200

    add output for IDs not found in the input list

--- ukbb/get_icd_status.py
+++ ukbb/get_icd_status.py
@@ -109,7 +109,12 @@ def main():
         df=df[df["eid"].isin(id_list)]
     df=df.groupby(["eid"],as_index=False).agg({icd_col:lambda x:list(x)})
     df[icd]=df.apply(lambda x: 1 if icd in x[icd_col] else 0,axis=1)
-    print(df[["eid",icd]].rename(columns={"eid":"ID"}).to_csv(sep="\t",index=False),end='') 
+    print(df[["eid",icd]].rename(columns={"eid":"ID"}).to_csv(sep="\t",index=False),end='')
+    if not(id_list is None) and len(id_list)!=0:
+        # IDs in input list but not in the previous output
+        not_found_ids=set(id_list)-set(df["eid"])
+        if len(not_found_ids)!=0:
+            print(pd.DataFrame({"A":list(not_found_ids),"B":"NA"}).to_csv(header=False,index=False,sep="\t"),end='')
         
 if __name__=="__main__":
     main()
