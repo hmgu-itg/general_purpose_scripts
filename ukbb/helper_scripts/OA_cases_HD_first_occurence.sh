44e1cf216101516096ce7ddc786f6cce3fc8b977
andrei <andrei@linux.fritz.box>
2023-10-11 03:57:25 +0200

    add -k, finalize

--- ukbb/helper_scripts/OA_cases_HD_first_occurence.sh
+++ ukbb/helper_scripts/OA_cases_HD_first_occurence.sh
@@ -17,6 +17,7 @@ function usage () {
     echo "                       -k | --key <one of: OA, FingerOA, HandOA, HipKneeOA, HipOA, KneeOA, SpineOA, ThumbOA>"
     echo "                       -o | --output <output prefix>"
     echo "                       -c | --config <optional: config file; default: config.txt in script directory>"
+    echo "                       -p | --keep <optional: keep temporary files>"
     echo "                       -h | --help"
     echo ""
     exit 0
@@ -34,14 +35,17 @@ eval set -- "$OPTS"
 
 declare -A keys=( [FingerOA]=1 [HandOA]=1 [HipKneeOA]=1 [HipOA]=1 [KneeOA]=1 [OA]=1 [SpineOA]=1 [ThumbOA]=1 )
 
+declare -a tempfiles
 config=""
 hesin_release=""
 outprefix=""
 key=""
 outfile=""
+keep="NO"
 while true; do
   case "$1" in
     -h|--help ) usage; shift ;;
+    -p|--keep ) keep="YES"; shift ;;
     -e|--hesin ) hesin_release=$2; shift 2 ;;
     -k|--key ) key=$2; shift 2 ;;
     -c|--config ) config=$2; shift 2 ;;
@@ -76,25 +80,12 @@ out_dir=$(dirname "$outfile")
 
 #----------------------------------------------------------------------------------------------------------------
 
-declare -a tempfiles
-# tempfiles["tmp_icd9_incl"]="tmp_XXXXXXX"
-# tempfiles["tmp_icd10_incl"]="tmp_XXXXXXX"
-# tempfiles["tmp_inclusion_out"]="tmp_XXXXXXX"
-# tempfiles["tmp_icd9_excl"]="tmp_XXXXXXX"
-# tempfiles["tmp_icd10_excl"]="tmp_XXXXXXX"
-# tempfiles["tmp_exclusion_out"]="tmp_XXXXXXX"
-
-# if createTempFiles "$out_dir" tempfiles;then
-#     echo "INFO: created temporary files"|tee -a "$logfile"
-# else
-#     echo "ERROR: failed to create temporary files"|tee -a "$logfile"
-#     exit 1
-# fi
-
 tmpdir=$(mktemp -d -p "$out_dir" cases_XXXXXXXX)
 if [[ $? -ne 0 ]];then
     echo "ERROR: could not create temp dir in $out_dir" | tee -a "$logfile"
     exit 1
+else
+    echo "INFO: temp dir: $tmpdir"    
 fi
 
 #----------------------------------------------------------------------------------------------------------------
@@ -106,6 +97,8 @@ echo "Command line: $scriptname ${args[@]}"|tee -a "$logfile"
 echo ""|tee -a "$logfile"
 echo "INFO: HESIN release: $hesin_release"|tee -a "$logfile"
 echo "INFO: key: $key"|tee -a "$logfile"
+echo "INFO: temp dir: $tmpdir"|tee -a "$logfile"
+echo "INFO: keep temp files: $keep"|tee -a "$logfile"
 echo "INFO: output prefix: $outprefix"|tee -a "$logfile"
 echo "INFO: output file: $outfile"|tee -a "$logfile"
 echo ""|tee -a "$logfile"
@@ -157,26 +150,9 @@ tail -n +2 "$merged_excl_fname" | perl -lne 'BEGIN{$,="\t";}{@a=split(/\t/);$id=
 
 #------------------------------------------------------------------------------------------------------------------
 
+join -t $'\t' -1 1 -2 1 -a 1 -a 2 -e "NA" -o 1.1,2.1,1.2,2.2 <(tail -n +2 "$tmpdir"/inclusion_final | sort -k1,1) <(tail -n +2 "$tmpdir"/exclusion_final | sort -k1,1) | awk 'BEGIN{FS=OFS="\t";}{if ($2=="NA"){$2=$1;}if ($4=="0"){print $2,$3;}}' > "$outfile"
 
-
-
-grep ^icd9 "$icd_exclusion_file"| cut -f 2 > "${tempfiles[tmp_icd9_excl]}"
-grep ^icd10 "$icd_exclusion_file"| cut -f 2 > "${tempfiles[tmp_icd10_excl]}"
-echo ""  | tee -a "$logfile"
-PYTHONPATH="${upperdir}"/python "$hesin_script" -p "OA" -r "$hesin_release" --icd9 "${tempfiles[tmp_icd9_excl]}" --icd10 "${tempfiles[tmp_icd10_excl]}" -o "${tempfiles[tmp_exclusion_out]}" > >(tee -a "$logfile") 2> >(tee -a "$logfile" >&2)
-
-# only output samples in inclusion that are not in exclusion
-join -1 1 -2 1 -a 1 -t$'\t' -e NULL -o 1.1,2.1 <(sort "${tempfiles[tmp_inclusion_out]}") <(sort "${tempfiles[tmp_exclusion_out]}") | grep NULL | cut -f 1  > "${outfile}"
-echo ""  | tee -a "$logfile"
-echo "INFO: final output list:" $(cat "${outfile}"| wc -l) "IDs" | tee -a "$logfile"
-echo ""  | tee -a "$logfile"
-
-# delete temp files
-for fn in "${tempfiles[@]}";do
-    if [[ -f "$fn" ]];then
-	rm "$fn"
-    fi
-done
+#------------------------------------------------------------------------------------------------------------------
 
 date "+%d-%b-%Y:%H-%M-%S"|tee -a "$logfile"
 
