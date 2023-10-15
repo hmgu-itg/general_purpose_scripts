beca37bff6fbd31ccf85f24fccf57c52a5235600
andrei <andrei@linux.fritz.box>
2023-10-15 05:17:40 +0200

    minor change

--- ukbb/first_operation_date.py
+++ ukbb/first_operation_date.py
@@ -12,29 +12,31 @@ from itgukbb import utils
 
 # given patient ID(s) and ICD code, get the date of the first diagnosis
 
-# epistart -> admidate -> epiend
-def calc_diagnosis_date(row):
-    if row["epistart"]=="NA":
-        if row["admidate"]=="NA":
-            if row["epiend"]=="NA":
-                return "NA"
+# opdate -> epistart -> admidate -> epiend
+def calc_operation_date(row):
+    if row["opdate"]=="NA":
+        if row["epistart"]=="NA":
+            if row["admidate"]=="NA":
+                if row["epiend"]=="NA":
+                    return "NA"
+                else:
+                    return row["epiend"]
             else:
-                return row["epiend"]
+                return row["admidate"]
         else:
-            return row["admidate"]
+            return row["epistart"]
     else:
-        return row["epistart"]
+        return row["opdate"]
 
 def main():
     verbosity=logging.INFO
 
-    parser=argparse.ArgumentParser(description="Given patient IDs and ICD code(s), get the date of the first diagnosis",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser=argparse.ArgumentParser(description="Given patient IDs and OPCS4 code(s), get the date of the first operation",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     rgroup=parser.add_argument_group("required arguments")
     rgroup.add_argument('--project','-p',required=True,action='store',help="Project name")
     rgroup.add_argument('--release','-r',required=True,action='store',help="Project HESIN release")
     group=rgroup.add_mutually_exclusive_group(required=True)
-    group.add_argument('--icd10','-icd10',required=False,action='store',help="ICD10 code or a file/list with ICD10 codes")
-    group.add_argument('--icd9','-icd9',required=False,action='store',help="ICD9 code or a file/list with ICD9 codes")
+    group.add_argument('--opcs4','-opcs4',required=False,action='store',help="OPCS4 code or a file/list with OPCS4 codes")
     # opgroup=parser.add_argument_group("optional arguments")
     parser.add_argument('--id','-id',required=False,action='store',help="Patient ID or a file/list with patient IDs")
     parser.add_argument('--output','-o',required=False,action='store',help="Output file")
@@ -53,8 +55,7 @@ def main():
     project=args.project
     release=args.release
     id_list=utils.readIDList(args.id)
-    icd10=utils.readIDList(args.icd10)
-    icd9=utils.readIDList(args.icd9)
+    opcs4=utils.readIDList(args.opcs4)
 
     if args.verbose is not None:
         if args.verbose=="debug":
@@ -99,37 +100,31 @@ def main():
         LOGGER.info("using all samples in input file")
     LOGGER.info("")
 
-    icd=icd9
-    icd_col="diag_icd9"
-    if icd9 is None:
-        icd=icd10
-        icd_col="diag_icd10"
-        
 #-----------------------------------------------------------------------------------------------------------------------------
 
     with tarfile.open(infile,"r:*") as tar:
         df_main=pd.read_table(tar.extractfile("hesin.txt"),sep="\t",header=0,dtype=str,quotechar='"',quoting=csv.QUOTE_NONE,keep_default_na=False,usecols=["eid","ins_index","epistart","epiend","admidate"])
-        df_diag=pd.read_table(tar.extractfile("hesin_diag.txt"),sep="\t",header=0,dtype=str,quotechar='"',quoting=csv.QUOTE_NONE,keep_default_na=False,usecols=["eid","ins_index",icd_col])
+        df_oper=pd.read_table(tar.extractfile("hesin_oper.txt"),sep="\t",header=0,dtype=str,quotechar='"',quoting=csv.QUOTE_NONE,keep_default_na=False,usecols=["eid","ins_index","opdate","oper4"])
         JT=pd.merge(df_main,df_diag,on=["eid","ins_index"],how="inner")
         if id_list is None or len(id_list)==0:
-            JT=JT[JT[icd_col].isin(icd)]
+            JT=JT[JT["oper4"].isin(opcs4)]
         else:
-            JT=JT[(JT[icd_col].isin(icd)) & (JT["eid"].isin(id_list))]
+            JT=JT[(JT["oper4"].isin(opcs4)) & (JT["eid"].isin(id_list))]
                 
         # print(JT.to_csv(sep="\t",index=False),end='',file=sys.stderr)
         if (len(JT)==0):
-            LOGGER.info("could not find any records for provided ICD codes / sample IDs")
+            LOGGER.info("could not find any records for provided OPCS4 codes / sample IDs")
         else:
-            JT["diagnosis_date"]=JT.apply(calc_diagnosis_date,axis=1)
-            JT["diagnosis_date_fmt"]=pd.to_datetime(JT["diagnosis_date"],dayfirst=True,errors="coerce")
+            JT["operation_date"]=JT.apply(calc_operation_date,axis=1)
+            JT["operation_date_fmt"]=pd.to_datetime(JT["operation_date"],dayfirst=True,errors="coerce")
             # only interested in the earliest date, NaT values are ignored
             # if for a sample there are only NaT values then this sample will not be in idx, so not reported
-            idx=JT.groupby(["eid"])["diagnosis_date_fmt"].transform(min)==JT["diagnosis_date_fmt"]
+            idx=JT.groupby(["eid"])["operation_date_fmt"].transform(min)==JT["operation_date_fmt"]
             # sometimes for the same ID and ICD there are multiple entries with the same date, so we need drop_duplicates
             if args.output:
-                JT[idx][["eid","diagnosis_date"]].drop_duplicates().rename(columns={"eid":"ID","diagnosis_date":"date"}).to_csv(args.output,sep="\t",index=False)
+                JT[idx][["eid","operation_date"]].drop_duplicates().rename(columns={"eid":"ID","operation_date":"date"}).to_csv(args.output,sep="\t",index=False)
             else:
-                print(JT[idx][["eid","diagnosis_date"]].drop_duplicates().rename(columns={"eid":"ID","diagnosis_date":"date"}).to_csv(sep="\t",index=False),end='')
+                print(JT[idx][["eid","operation_date"]].drop_duplicates().rename(columns={"eid":"ID","operation_date":"date"}).to_csv(sep="\t",index=False),end='')
             if not(id_list is None) and len(id_list)!=0:
                 # IDs in input list but not in the previous output
                 not_found_ids=not_found_ids.union(set(id_list)-set(JT[idx]["eid"]))
