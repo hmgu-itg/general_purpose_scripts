c36fb40b6be3cf294fd5b89307d93b1f1e8355dd
Andrei Barysenka <andrei.barysenka@suanpan.scidom.de>
2025-02-05 13:53:08 +0100

    rm ukbb

--- ukbb/removeEmpty.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-$\="\n";
-while(<STDIN>){
-    chomp;
-    @a=split(/\t/,$_,-1);
-    for ($i=0;$i<scalar(@a);$i++){$a[$i]="NA" if length($a[$i])==0;}
-    print join("\t",@a);
-}
